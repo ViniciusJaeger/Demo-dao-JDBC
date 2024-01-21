d7abd2540b3ae75e5b2c79a8aee9a6512e3730c9
Vinicius Jaeger <viniciusjaegerprogramador@gmail.com>
2024-01-21 16:17:03 -0300

    Department entity class

---
+++ src/application/Programm.java
@@ -0,0 +1,15 @@
+package application;
+
+import models.entities.Department;
+
+public class Programm {
+
+	public static void main(String[] args) {
+		
+		Department obj = new Department(1, "Gym");
+		System.out.println(obj);
+	
+
+	}
+
+}
